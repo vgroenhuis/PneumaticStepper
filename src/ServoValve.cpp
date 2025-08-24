5778926051bf859969b4c2282528c84607df3254
Vincent Groenhuis <groenhuis@gmail.com>
2025-08-24 21:20:24 +0200

    Fixed ServoValve issues after splitting it in .h and .cpp

--- src/ServoValve.cpp
+++ src/ServoValve.cpp
@@ -8,10 +8,13 @@ using namespace std;
 // by the testing framework.
 extern unsigned long millis();
 extern unsigned long micros();
-
+extern long map(long x, long in_min, long in_max, long out_min, long out_max);
+/* 
+// example implementation:
 long map(long x, long in_min, long in_max, long out_min, long out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+*/
 #endif
 
 #include "ServoValve.h"
